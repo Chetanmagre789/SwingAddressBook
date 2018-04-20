352f0a2e5b26497ac7ed14f5ba67f948beff3d09
chetan.magre <chetan.magre@YI1007111DT.Yash.com>
2018-04-20 13:57:30 +0530

    initial commit

---
+++ src/test/java/abook/service/ABServiceTest.java
@@ -0,0 +1,25 @@
+package abook.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import abook.pojo.User;
+
+public class ABServiceTest {
+	
+	private ABService aBService;
+	
+	@Before
+	public void init(){
+		aBService= new ABService();
+	}
+	
+	@Test
+	public void test_case_for_authanticateUser_method_if_correct_Return_User_object() {
+		
+		assertArrayEquals(aBService.authanticateUser("a", "a"), User.class);
+	}
+
+}
